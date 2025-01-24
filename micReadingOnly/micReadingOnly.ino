1843b1976329a738dfe720110bc17ed431a65441
zhenloopy <145357830+zhenloopy@users.noreply.github.com>
2025-01-24 08:27:31 -0600

    Switched transmit times

--- micReadingOnly/micReadingOnly.ino
+++ micReadingOnly/micReadingOnly.ino
@@ -4,9 +4,9 @@ const short MIC_PIN = 13; // the pin we connect the MIC to
 int currentState; // what the new state is
 int pastState = HIGH; // what the past state was
 unsigned long previousMillis = 0; // how we tell when we last switched state
-unsigned long shortInterval = 500; // how we differentiate between short + long, in milliseconds so this is .5 seconds
+unsigned long shortInterval = 700; // how we differentiate between short + long, in milliseconds so this is .5 seconds
+unsigned long longInterval = 2000;
 const unsigned long debounceInterval = 50;  // Ignore rapid changes under 50ms
-bool changedRecently = 1;
 
 void setup() {
   Serial.begin(115200);
@@ -21,12 +21,13 @@ void loop() {
     unsigned long timeDifference = currentMillis - previousMillis;
 
     if (timeDifference <= shortInterval) {
-      changedRecently = 1;
-      Serial.println("Short change detected");
+      Serial.println("DOT");
+    } else if (timeDifference <= longInterval) {
+      Serial.println("DASH");
     } else {
-      changedRecently = 0;
-      Serial.println("Long change detected");
+      Serial.println("IGNORE");
     }
+    
 
     previousMillis = currentMillis;
   }
