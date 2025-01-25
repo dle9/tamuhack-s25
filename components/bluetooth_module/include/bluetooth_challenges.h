f50cd7ea032ae17c3e23544b52e96a339d0e09a5
Oscar Leon <leono@tamu.edu>
2025-01-25 15:34:24 -0600

    Fixed CMakeFiles

--- components/bluetooth_module/include/bluetooth_challenges.h
+++ components/bluetooth_module/include/bluetooth_challenges.h
@@ -2,11 +2,13 @@
 #pragma once
 
 #include "esp_err.h"
-#include "esp_bt.h"
+#include "esp_bt_main.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gatts_api.h"
-#include "esp_bt_defs.h"
-#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
+#include "esp_bt_device.h"
+#include "esp_gatt_defs.h"
+#include "esp_system.h"
 
 // Types of Bluetooth security challenges
 typedef enum {
